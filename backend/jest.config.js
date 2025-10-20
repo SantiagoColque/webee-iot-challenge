b1e692bbd85c8dc756568bba4763561ed5975698
Santiago Colque <santiago.colque@mi.unc.edu.ar>
2025-10-20 20:15:17 -0300

    feat: configurar testing y linting para backend
    - Agregar Jest para tests unitarios
    - Configurar Supertest para tests de API
    - Agregar ESLint con configuración estándar
    - Incluir scripts de test, coverage y linting

---
+++ backend/jest.config.js
@@ -0,0 +1,21 @@
+{
+  "testEnvironment": "node",
+  "testMatch": [
+    "**/__tests__/**/*.js",
+    "**/?(*.)+(spec|test).js"
+  ],
+  "collectCoverageFrom": [
+    "**/*.js",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+    "!jest.config.js"
+  ],
+  "coverageDirectory": "coverage",
+  "coverageReporters": [
+    "text",
+    "lcov",
+    "html"
+  ],
+  "setupFilesAfterEnv": ["<rootDir>/test/setup.js"],
+  "testTimeout": 10000
+}
